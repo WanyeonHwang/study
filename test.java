3b4cb2d1977a9397feae24382da05d68592dd796
wanyen <wanyen@gmail.com>
2018-01-06 10:40:53 +0900

    [study][test] check conflict 2
    [SV]
    [DEC] check conflict 2

--- test.java
+++ test.java
@@ -2,7 +2,16 @@ import java.util.*;
 
 class test {
     public static void main(String[] args) {
+        System.out.println("test case 0");
+
         System.out.println("hello world");
-        System.out.println("TEST");
+        System.out.println("test case 1");
+
+        System.out.println("test case 2");
+        test();
+    }
+
+    static void test() {
+        System.out.println("test method");
     }
 }
